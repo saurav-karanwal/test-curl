24dc2904630eb2fdc8a4bfc93a16ea2294f0f715
saurav-karanwal <saurav.karanwal@simnovus.com>
2022-12-08 10:14:23 +0530

    added curl verison

--- install_curl.sh
+++ install_curl.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+CURL_VER=7.69.1
 # Install any build dependencies needed for curl
 #sudo apt-get build-dep curl
 
 # Get latest libcurl
 mkdir ~/curl
 cd ~/curl
-wget http://curl.haxx.se/download/curl-7.69.1.tar.bz2
-tar -xvjf curl-7.69.1.tar.bz2
-cd curl-7.69.1
+wget http://curl.haxx.se/download/curl-$CURL_VER.tar.bz2
+tar -xvjf curl-$CURL_VER.tar.bz2
+cd curl-$CURL_VER
 
 # The usual steps for building an app from source
 ./configure
